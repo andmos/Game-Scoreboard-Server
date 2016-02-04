c64252f36252decb4f22d4f2a232e41dbcb0de91
andmos <andreas.mosti@gmail.com>
2016-02-04 21:16:17 +0100

    trying to run integrationtests on travis

--- buildServer.sh
+++ buildServer.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash
 
 # I guess I will scrap this later.
 
@@ -11,6 +11,27 @@ function build-local()
 function build-docker()
 {
     sudo docker build -t gamescoreboardserver .
+}
+
+function run-docker()
+{
+    sudo docker run -dt -p 5000:5000 gamescoreboardserver
+}
+
+function run-dockerAndLinkToDb()
+{
+    cd Database
+    chmod +x setUpDockerMysqlDatabase
+    ./setUpDockerMysqlDatabase
+    sudo docker build -t gamescoreboardserver ..
+    sudo docker run -dt --link gamescoreboarddb:gamescoreboarddb -p 5000:5000 gamescoreboardserver
+    cd ..
+}
+
+function run-integration-docker()
+{
+   run-dockerAndLinkToDb
+   mono GameScoreboardServer/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe GameScoreboardServer/TestGameScoreboardServer/bin/Debug/Test*.dll
 }
 
 
