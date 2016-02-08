9c99cb3fb4ea0cdde08f03cf3ac3f6797bfbd0d7
andmos <andreas.mosti@gmail.com>
2016-02-08 22:19:37 +0100

    yeah. 12 chars was to low

--- Database/createGameScoreboardDatabase.sql
+++ Database/createGameScoreboardDatabase.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `GameScoreBoard` (
-    `GameName` varchar(11) DEFAULT NULL,
-    `PlayerName` varchar(11) DEFAULT NULL,
-    `Score` int(11) unsigned,
+    `GameName` varchar(15) DEFAULT NULL,
+    `PlayerName` varchar(15) DEFAULT NULL,
+    `Score` int(15) unsigned,
     `recordId` int(11) unique key AUTO_INCREMENT,
     PRIMARY KEY (`recordId`))
 ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
