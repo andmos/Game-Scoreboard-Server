6d9906d3c0ca846152ad09ff8e5d05d75792b1cd
andmos <andreas.mosti@gmail.com>
2016-02-10 22:00:57 +0100

    mocked IDatastorage for modules tests

--- GameScoreboardServer/TestGameScoreboardServer/Modules/TestScoreboardModule.cs
+++ GameScoreboardServer/TestGameScoreboardServer/Modules/TestScoreboardModule.cs
@@ -5,18 +5,15 @@ using Nancy;
 using LightInject.Nancy;
 using Nancy.Testing;
 using LightInject;
-using Newtonsoft.Json; 
+using Newtonsoft.Json;
 using GameScoreboardServer.Crypto;
 using System.Collections.Generic;
 using GameScoreboardServer.Models;
+using Moq;
+using GameScoreboardServer.Services;
 
 namespace TestGameScoreboardServer.Modules
 {
-
-	/*
-		Tests are now using database, need to mock this shit yo! 
-		Tagged as integration until then
-	*/
 
 	[TestFixture()]
 	public class TestScoreboardModule
@@ -24,84 +21,119 @@ namespace TestGameScoreboardServer.Modules
 		[Test()]
 		[Category("unit")]
 		public void Ping_GivenCorrectRoute_ReturnsPong()
-		{ 
-			var bootstrapper = new LightInjectNancyBootstrapper ();
-			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json")); 
+		{
+			var bootstrapper = new LightInjectNancyBootstrapper();
+			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
 
-			var result = browser.Get("/api/v1/ping", with => 
+			var result = browser.Get("/api/v1/ping", with =>
 				{
 					with.HttpRequest();
 				});
-			
-			Assert.AreEqual (HttpStatusCode.OK, result.StatusCode); 
+
+			Assert.AreEqual (HttpStatusCode.OK, result.StatusCode);
 			Assert.AreEqual ("pong", result.Body.AsString());
 		}
-		
+
 		[Test()]
-		[Category("integration")]
+		[Category("unit")]
 		public void GameScoreBoard_GivenValidGameNameAndCount_ReturnsCorrectJson()
 		{
-			var bootstrapper = new LightInjectNancyBootstrapper ();
-			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json")); 
-			string expectedGameName = "game1"; 
-			int expectedNumberOfResults = 5; 
+			var bootstrapper = new TestableLightInjectNancyBootstrapper ();
+			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
+			string expectedGameName = "game1";
+			int expectedNumberOfResults = 1;
 
-			var response = browser.Get("/api/v1/gameScoreBoard", with => 
+			var response = browser.Get("/api/v1/gameScoreBoard", with =>
 				{
 					with.HttpRequest();
 					with.Query("gameName", expectedGameName);
-					with.Query("count", expectedNumberOfResults.ToString()); 
+					with.Query("count", expectedNumberOfResults.ToString());
 				});
 			var responseModels = JsonConvert.DeserializeObject<IEnumerable<ScoreRecord>> (response.Body.AsString());
 
 			Assert.IsTrue (responseModels.Count() == expectedNumberOfResults);
-			Assert.AreEqual (expectedGameName, responseModels.FirstOrDefault ().GameName); 
+			Assert.AreEqual (expectedGameName, responseModels.FirstOrDefault ().GameName);
 		}
 
 		[Test()]
-		[Category("integration")]
+		[Category("unit")]
 		public void PlayerScoreBoard_GivenValidUsername_ReturnsCorrectJson()
 		{
-			var bootstrapper = new LightInjectNancyBootstrapper ();
-			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json")); 
-			string expectedPlayerName = "player1"; 
+			string expectedPlayerName = "player1";
+			var mockStorage = new Mock<IDataStorage> ();
+			mockStorage.Setup(x => x.GetAllScoresForUsername(expectedPlayerName)).Returns(new List<ScoreRecord>() { new ScoreRecord {PlayerName = expectedPlayerName}});
+			var bootstrapper = new TestableLightInjectNancyBootstrapper(mockStorage.Object);
+			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
 
-			var response = browser.Get("/api/v1/playerScoreBoard", with => 
+
+			var response = browser.Get("/api/v1/playerScoreBoard", with =>
 				{
 					with.HttpRequest();
-					with.Query("playerName", expectedPlayerName); 
+					with.Query("playerName", expectedPlayerName);
 				});
 			var responseModels = JsonConvert.DeserializeObject<IEnumerable<ScoreRecord>> (response.Body.AsString());
 
-			Assert.IsTrue (responseModels.Count() >= 1); 
-			Assert.AreEqual (expectedPlayerName, responseModels.FirstOrDefault().PlayerName); 
+			Assert.IsTrue (responseModels.Count() >= 1);
+			Assert.AreEqual (expectedPlayerName, responseModels.FirstOrDefault().PlayerName);
 		}
 
 
 		[Test()]
-		[Category("integration")]
+		[Category("unit")]
 		public void AddScoreBoardData_GivenValidGameRecordObjectAsJson_ReturnsHttpCreated()
 		{
-			var bootstrapper = new LightInjectNancyBootstrapper ();
-			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json")); 
+			var bootstrapper = new TestableLightInjectNancyBootstrapper ();
+			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
 			var scoreRecordObject = new ScoreRecord {
 				GameName = "game5",
 				PlayerName = "player5",
 				Score = 5000
-			}; 
+			};
 
 			var response = browser.Post ("/api/v1/addScoreBoardData", with => {
 				with.Header("Content-Type", "application/json");
-				with.JsonBody (scoreRecordObject); 
+				with.JsonBody (scoreRecordObject);
 			});
 
-			Assert.AreEqual (HttpStatusCode.Created, response.StatusCode); 
-		
+			Assert.AreEqual (HttpStatusCode.Created, response.StatusCode);
+
+		}
+
+		[Test()]
+		[Category("unit")]
+		public void AddScoreBoardData_GivenValidGameRecordObjectAsJson_ReturnsCorrectObjectFromStorage()
+		{
+			var bootstrapper = new TestableLightInjectNancyBootstrapper ();
+			var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
+			string expectedGameName = "game5";
+			int expectedCount = 1;
+			var scoreRecordObject = new ScoreRecord {
+				GameName = expectedGameName,
+				PlayerName = expectedCount.ToString(),
+				Score = 5000
+			};
+
+			var post = browser.Post ("/api/v1/addScoreBoardData", with => {
+				with.Header("Content-Type", "application/json");
+				with.JsonBody (scoreRecordObject);
+			});
+
+			var response = browser.Get("/api/v1/gameScoreBoard", with =>
+				{
+					with.HttpRequest();
+					with.Query("gameName", expectedGameName);
+					with.Query("count", expectedCount.ToString());
+				});
+			var responseModels = JsonConvert.DeserializeObject<IEnumerable<ScoreRecord>> (response.Body.AsString());
+
+			Assert.IsTrue (responseModels.Count() == expectedCount);
+			Assert.AreEqual (expectedGameName, responseModels.FirstOrDefault ().GameName);
+
+
 		}
 
 
-	
-	}
-		
-}
 
+	}
+
+}
---
+++ GameScoreboardServer/TestGameScoreboardServer/Modules/TestableLightInjectNancyBootstrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using LightInject.Nancy;
+using LightInject;
+using GameScoreboardServer;
+using GameScoreboardServer.Services;
+using GameScoreboardServer.Models;
+using System.Collections.Generic;
+using GameScoreboardServer.Crypto;
+
+namespace TestGameScoreboardServer
+{
+	public class TestableLightInjectNancyBootstrapper : LightInjectNancyBootstrapper 
+	{
+
+		IDataStorage m_dataStorage; 
+
+		public TestableLightInjectNancyBootstrapper(IDataStorage dataStorage = null)
+		{
+			if (dataStorage != null) 
+			{
+				m_dataStorage = dataStorage; 
+			} else 
+			{
+				m_dataStorage = new GameScoreBoardDataCache (); 
+				AddMultipleRecordsToCache (m_dataStorage); 
+			}
+		}
+
+		protected override IServiceContainer GetServiceContainer()
+		{
+
+			IServiceContainer container = new ServiceContainer (); 
+			container.RegisterInstance<IDataStorage> (m_dataStorage); 
+			container.Register<ICryptation, StringCipher> (); 
+
+			return container; 
+		}
+
+		private void AddMultipleRecordsToCache(IDataStorage cache)
+		{
+			var records = new List<ScoreRecord> () 
+			{
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 5000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 4000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 6000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 3000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 8000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 7000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 9000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 8000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 4000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 7000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 8000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 8000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player1", Score = 9000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player2", Score = 11000 }, 
+				new ScoreRecord { GameName = "game1", PlayerName = "player3", Score = 1000 }, 
+			};
+			foreach (var record in records) 
+			{
+				cache.AddScoreRecordToStorage(record); 
+			}
+		}
+	}
+}
+
